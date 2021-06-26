b0dea8bf18fd49168560ae7aad495bd039b48771
khadija <khadijamahanga@y7mail.com>
2021-06-26 15:46:14 +0300

    clean up and next conf set-up

---
+++ next-seo.config.js
@@ -0,0 +1,27 @@
+export default {
+  titleTemplate: "%s | Jiko Point",
+  defaultTitle: "Jiko Class",
+  description:
+    "Jiko Point's class and news",
+  openGraph: {
+    type: "website",
+    url: "https://www.jikoni.co.tz",
+    locale: "en_NG",
+    site_name: "Jiko Point",
+    images: [
+      {
+        /* eslint-disable global-require */
+        url: "/icons/icon-512x512.png",
+        width: 512,
+        height: 512,
+        alt: "Jiko Point",
+      },
+    ],
+  },
+  additionalMetaTags: [
+    {
+      name: "viewport",
+      content: "minimum-scale=1, initial-scale=1, width=device-width",
+    },
+  ],
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,1 +1,45 @@
-<meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { DefaultSeo } from "next-seo";
+import NextNprogress from "nextjs-progressbar";
+import PropTypes from "prop-types";
+import React from "react";
+
+import theme from "@/jikopoint/theme";
+import SEO from "next-seo.config";
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <NextNprogress
+          color={theme.palette.primary.light}
+          startPosition={0.2}
+          stopDelayMs={200}
+          height={2}
+          showOnShallow
+        />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.shape({}).isRequired,
+};
