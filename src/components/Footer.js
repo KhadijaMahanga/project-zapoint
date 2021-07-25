7b32f3ef9ca7642cefaf3ab8752979b9f24888e0
khadija <khadijamahanga@y7mail.com>
2021-07-25 13:04:43 +0300

    footer responsiveness complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
-import { IconButton, Grid } from "@material-ui/core";
+import { Divider, Hidden, IconButton, Grid, List, ListItemText, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import React from "react";
+import PropTypes from "prop-types";
+import React, { Fragment } from "react";
 
 import { ReactComponent as Logo } from "@/jikopoint/assets/logos/logo-foodlab-white.svg";
 import { ReactComponent as FacebookIcon } from "@/jikopoint/assets/icons/icon-facebook-white.svg";
@@ -15,13 +16,16 @@ import Section from "@/jikopoint/components/Section";
 const useStyles = makeStyles(({ breakpoints, palette, typography }) => ({
   root: {
       backgroundColor: palette.background.dark,
-      padding: `${typography.pxToRem(30)} 0`,
-      [breakpoints.up("lg")]: {
-          padding: `${typography.pxToRem(50)} 0`,
-      }
+  },
+  belowBanner: {
+    borderTop: "1px solid #595959",
+    padding: typography.pxToRem(12),
   },
   logoSocial: {
-      padding: typography.pxToRem(15),
+    padding: `${typography.pxToRem(45)} ${typography.pxToRem(15)}`,
+    [breakpoints.up("lg")]: {
+        padding: `${typography.pxToRem(65)} ${typography.pxToRem(15)}`,
+    },
   },
   button: {
     borderRadius: "50%",
@@ -45,69 +49,139 @@ const useStyles = makeStyles(({ breakpoints, palette, typography }) => ({
     justifyContent: "center",
     display: "flex",
   },
+  divider: {
+    backgroundColor: "#5F5F5F",
+    float: "left",
+    height: typography.pxToRem(14),
+    margin: `auto ${typography.pxToRem(20)}`,
+  },
+  list: {
+    listStyle: "none",
+    display: "block",
+    padding: 0,
+  },
+  listItemLink: {
+    float: "left",
+    color: palette.highlight.main,
+    fontSize: typography.pxToRem(14),
+    fontWeight: 400,
+    textTransform: "uppercase",
+  },
+  listItemText: {
+    margin: 0,
+  },
+  flexDisplay: {
+    height: typography.pxToRem(18),
+  }
 }));
 
-function Footer({ social }) {
+function ListItemLink(props) {
+  const classes = useStyles();
+  return (
+    <Link {...props} variant="subtitle2" className={classes.listItemLink} />
+  );
+}
+
+function Footer({ social, footerItems }) {
   const classes = useStyles();
   return (
     <div className={classes.root}>
         <Section>
             <Grid container justify="center" alignItems="center" direction="column" className={classes.logoSocial}>
                 <Grid item>
-                <IconButton
-                    component={Link}
-                    href={"/"}
-                    underline="none"
-                >
-                    <Logo className={classes.logo} viewBox="156 0 180 100" />
-                </IconButton>
+                  <IconButton
+                      component={Link}
+                      href={"/"}
+                      underline="none"
+                  >
+                      <Logo className={classes.logo} viewBox="156 0 180 100" />
+                  </IconButton>
                 </Grid>
                 <Grid item className={classes.social}>
-                <IconButton
-                    component={Link}
-                    className={classes.button}
-                    href={social.facebook}
-                    underline="none"
-                >
-                    <FacebookIcon className={classes.icon} />
-                </IconButton>
-                <IconButton
-                    component={Link}
-                    className={classes.button}
-                    href={social.twitter}
-                    underline="none"
-                >
-                    <TwitterIcon className={classes.icon} />
-                </IconButton>
-                <IconButton
-                    component={Link}
-                    className={classes.button}
-                    href={social.linkedin}
-                    underline="none"
-                >
-                    <LinkedInIcon className={classes.icon} />
-                </IconButton>
-                <IconButton
-                    component={Link}
-                    className={classes.button}
-                    href={social.instagram}
-                    underline="none"
-                >
-                    <InstagramIcon className={classes.icon} viewBox="0 0 500 500" />
-                </IconButton>
-                <IconButton
-                    component={Link}
-                    className={classes.button}
-                    href={social.youtube}
-                    underline="none"
-                >
-                    <YoutubeIcon className={classes.icon} />
-                </IconButton>
+                  <IconButton
+                      component={Link}
+                      className={classes.button}
+                      href={social.facebook}
+                      underline="none"
+                  >
+                      <FacebookIcon className={classes.icon} />
+                  </IconButton>
+                  <IconButton
+                      component={Link}
+                      className={classes.button}
+                      href={social.twitter}
+                      underline="none"
+                  >
+                      <TwitterIcon className={classes.icon} />
+                  </IconButton>
+                  <IconButton
+                      component={Link}
+                      className={classes.button}
+                      href={social.linkedin}
+                      underline="none"
+                  >
+                      <LinkedInIcon className={classes.icon} />
+                  </IconButton>
+                  <IconButton
+                      component={Link}
+                      className={classes.button}
+                      href={social.instagram}
+                      underline="none"
+                  >
+                      <InstagramIcon className={classes.icon} viewBox="0 0 500 500" />
+                  </IconButton>
+                  <IconButton
+                      component={Link}
+                      className={classes.button}
+                      href={social.youtube}
+                      underline="none"
+                  >
+                      <YoutubeIcon className={classes.icon} />
+                  </IconButton>
                 </Grid>
             </Grid>
+            <Grid container alignItems="center" justify="space-between" className={classes.belowBanner}>
+                <Grid item>
+                  <Typography variant="caption">© Copyright 2021 JikoPoint</Typography>
+                </Grid>
+               <Hidden smDown implementation="css">
+                 <Grid item className={classes.flexDisplay}>
+                  <List component="nav" className={classes.list}>
+                    {footerItems.map(({ href, label }, index) => (
+                      <Fragment key={href}>
+                      <ListItemLink
+                        underline="none"
+                        href={href}
+                      >
+                        <ListItemText disableTypography className={classes.listItemText}>
+                          {label}
+                        </ListItemText>
+                      </ListItemLink>
+                      { (index + 1) !== footerItems.length && <Divider orientation="vertical" flexItem classes={{ root: classes.divider }} />}
+                      </Fragment>
+                    ))}
+                  </List>
+                </Grid>
+              </Hidden>
+            </Grid>
+
         </Section>
     </div>
   );
 }
+Footer.propTypes = {
+    footerItems: PropTypes.arrayOf(
+        PropTypes.shape({
+        label: PropTypes.string,
+        href: PropTypes.string,
+        })
+    ),
+    social: PropTypes.shape({}),
+};
+
+Footer.defaultProps = {
+    footerItems: undefined,
+    social: undefined,
+};
 
 export default Footer;
--- src/theme/index.js
+++ src/theme/index.js
@@ -8,8 +8,8 @@ const theme = createMuiTheme({
   palette: {
     primary: { main: "#0050FF" },
     secondary: { main: "#68B78C" },
-    highlight: { main: "#F9FF71" },
-    text: { primary: "#727475", secondary: "#fff" },
+    highlight: { main: "#a3a3a3" },
+    text: { primary: "#727475", secondary: "#fff", highlight: "#a3a3a3" },
     background: {
       default: "#fff",
       light: "#ceb28d",
@@ -108,6 +108,7 @@ const theme = createMuiTheme({
     caption: {
       fontFamily: FONT_FAMILY_TEXT,
       fontStretch: "normal",
+      color: "#a3a3a3",
       fontStyle: "normal",
       letterSpacing: 0,
     },
@@ -258,28 +259,8 @@ deepmerge(
       },
     },
     caption: {
-      fontSize: pxToRem(16),
-      lineHeight: 24 / 16,
-      [breakpoints.up("md")]: {
         fontSize: pxToRem(14),
         lineHeight: 20 / 14,
-      },
-      [breakpoints.up("xl")]: {
-        fontSize: pxToRem(20),
-        lineHeight: 30 / 20,
-      },
-    },
-    overline: {
-      fontSize: pxToRem(16),
-      lineHeight: 24 / 16,
-      [breakpoints.up("md")]: {
-        fontSize: pxToRem(14),
-        lineHeight: 20 / 14,
-      },
-      [breakpoints.up("xl")]: {
-        fontSize: pxToRem(20),
-        lineHeight: 30 / 20,
-      },
     },
   },
   { clone: false }
