f9dc1852586a175e62fff373c9c93d9c5e47d57c
khadija <khadijamahanga@y7mail.com>
2021-07-25 21:13:50 +0300

    fix hello api page

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { createHandler } from "@/jikopoint/middleware";
+import createHandler from "@/jikopoint/middleware";
 import User from "@/jikopoint/models/User";
 // you can pass in middleware here
 // maybe you have some permissions middleware
