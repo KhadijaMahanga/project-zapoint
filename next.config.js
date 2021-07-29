8981770e94793567006249ee6f33186b834732e0
khadija <khadijamahanga@y7mail.com>
2021-07-29 23:18:59 +0300

    hero component

--- next.config.js
+++ next.config.js
@@ -1,51 +1,27 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-const withImages = require("next-images");
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
-module.exports = withBundleAnalyzer(
-  withPWA(
-    withImages({
-      cssLoaderOptions: {
-        url: false,
-      },
-      // Exclude SVG from default file extensions list
-      // See: https://github.com/twopluszero/next-images/blob/master/index.js
-      fileExtensions: [
-        "jpg",
-        "jpeg",
-        "png",
-        "gif",
-        "ico",
-        "webp",
-        "jp2",
-        "avif",
+module.exports = withPWA({
+  images: {
+    domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS.split(", "),
+  },
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        "@svgr/webpack",
+        {
+          loader: "svg-url-loader",
+          options: {},
+        },
       ],
-      // images: {
-      //   domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS.split(", "),
-      // },
-      webpack(config) {
-        config.module.rules.push({
-          test: /\.svg$/,
-          use: [
-            "@svgr/webpack",
-            {
-              loader: "svg-url-loader",
-              options: {},
-            },
-          ],
-        });
-        return config;
-      },
-    }),
-    {
-      pwa: {
-        dest: "public",
-        disable: process.env.NODE_ENV === "development",
-        runtimeCaching,
-      },
-    }
-  )
-);
+    });
+    return config;
+  },
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    runtimeCaching,
+  },
+});
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 
+import Hero from "@/jikopoint/components/Hero";
 import Page from "@/jikopoint/components/Page";
 
 export default function Index() {
   return (
     <Page>
-      <div>HomePage</div>
+      <Hero
+        title="Jikoni"
+        subtitle={["kupika pilau", "kuchoma chapati", "kukanda ngano"]}
+        tagline="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas magna at porttitor vehicula. Nullam augue augue, dignissim id bibendum id, consequat et leo. Curabitur viverra tincidunt nulla nec tempor nullam augue augue."
+      />
     </Page>
   );
 }
