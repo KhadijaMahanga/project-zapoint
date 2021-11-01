3511d1b1349fd87297c4f5a3d353043c0a359abd
khadija <khadijamahanga@y7mail.com>
2021-11-01 10:04:27 +0300

    google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,17 +2,36 @@ import { CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { Provider as AuthProvider } from "next-auth/client";
 import { DefaultSeo } from "next-seo";
+import { useRouter } from "next/router";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useEffect } from "react";
 
 import "@/jikopoint/theme/fonts.css";
+import * as ga from "@/jikopoint/lib/ga";
 import theme from "@/jikopoint/theme";
 import SEO from "next-seo.config";
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
 
-  React.useEffect(() => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    // When the component is mounted, subscribe to router changes
+    // and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
