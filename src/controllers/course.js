bc2d494ff54f7c0b7e7e318965109dc3825f5f54
khadija <khadijamahanga@y7mail.com>
2021-11-26 11:44:38 +0300

    sort courses

--- src/controllers/course.js
+++ src/controllers/course.js
@@ -21,6 +21,7 @@ export const getAllCourses = async () => {
 
 export const getCourses = async () => {
   const res = await Course.find({ isArchived: false, status: "approved" })
+    .sort({ createdAt: 1 })
     .then((courses) => courses)
     .catch((e) => new Error(e));
 
