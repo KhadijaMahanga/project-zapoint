f86dc1e89f209c2bf096c0a7c6d91dec2062924a
KhadijaMahanga <khadijamahanga@y7mail.com>
2021-12-28 16:26:57 +0300

    send email through API

--- src/pages/api/comments/index.js
+++ src/pages/api/comments/index.js
@@ -4,7 +4,6 @@ import nc from "next-connect";
 import { createComment } from "@/jikopoint/controllers/comment";
 import middleware from "@/jikopoint/middleware";
 import { onNoMatch, onError } from "@/jikopoint/utils/handlers";
-import sendEmail from "@/jikopoint/utils/sendEmail";
 
 const handler = nc({ onNoMatch, onError })
   .use(middleware) // currently only database in middleware
@@ -14,12 +13,8 @@ const handler = nc({ onNoMatch, onError })
       if (!session && session?.user) {
         throw new Error("Hiki kitendo hakijathibitishwa");
       }
-      const { subject, email, content } = req?.body;
       const comment = await createComment(req?.body);
       res.json({ success: true, data: comment });
-      if (comment) {
-        await sendEmail({ subject, email, content });
-      }
     } catch (e) {
       console.log(e);
       res.status(401).send({ message: e, success: false });
