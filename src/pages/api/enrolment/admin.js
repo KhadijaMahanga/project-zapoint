85888abe71b324f1efd4484cb2b2833ed0dbae09
Khadija Mahanga <khadijamahanga@y7mail.com>
2022-06-08 12:47:54 +0300

    only admin can see enrolments

--- src/pages/api/enrolment/admin.js
+++ src/pages/api/enrolment/admin.js
@@ -1,4 +1,5 @@
 /* eslint-disable consistent-return */
+import { getSession } from "next-auth/react";
 import nextConnect from "next-connect";
 
 import { getAllEnrolments } from "@/jikopoint/controllers/enrolment";
@@ -8,13 +9,21 @@ import { onNoMatch, onError } from "@/jikopoint/utils/handlers";
 const handler = nextConnect({ onNoMatch, onError })
   .use(middleware)
   .get(async (req, res) => {
-    const enrolments = await getAllEnrolments();
-    if (!enrolments) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Enrolments not found" });
+    try {
+      const session = await getSession({ req });
+      if (!session && session?.user?.role !== "admin") {
+        throw new Error("Hiki kitendo hakijathibitishwa");
+      }
+      const enrolments = await getAllEnrolments();
+      if (!enrolments) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Enrolments not found" });
+      }
+      res.json({ success: true, data: enrolments });
+    } catch (e) {
+      res.status(401).json({ message: e, success: false });
     }
-    res.json({ success: true, data: enrolments });
   });
 
 export default handler;
